9bd7f9ee397172dd0e91a3207a5fe802794ec89b
alla karpenko <karpenkoallo4ka@gmail.com>
2020-12-29 23:58:17 +0200

    up

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,27 @@
-const objB = {
-    z: 10,
+
+
+const Account = function ({ login, email }) {
+    this.login = login;
+    this.email = email;
+}
+
+Account.prototype.getInfo = function () {
+    console.log(`Login: ${this.login}, Email: ${this.email}`);
+    
 };
-const objA = Object.create(objB);
-console.log(objA);
+
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
